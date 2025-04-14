6c2381d96a541ada5001ca8426e3fd031b905a94
Daniel Jantos <danjantos@gmail.com>
2025-04-14 17:43:08 -0400

    error with await data.json() function

--- app/posts/page.js
+++ app/posts/page.js
@@ -1,5 +1,31 @@
-export defalut function Posts()
-{
-    const data = 
+import styles from "./styles.modules.css";
 
+export default async function Posts()
+{
+    const data = fetch("http://localhost:4000/posts");
+    const posts = await data.json();
+
+    return (
+        <div>
+            <h1>Table for Posts</h1>
+            <table className={styles.posts_table}>
+                <thread>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                    </tr>
+                </thread>
+                <tbody>
+                    {
+                        posts.map( (post) => (
+                            <tr>
+                                <td>{post.id}</td>
+                                <td>{post.title}</td>
+                            </tr>
+                        )   )
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
 }
